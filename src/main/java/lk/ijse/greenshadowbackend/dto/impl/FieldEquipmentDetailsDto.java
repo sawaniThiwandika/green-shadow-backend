179b070e1e5cb629145f2addd2d77c4792bc2806
User <thiwandika.whs02@gmail.com>
2024-11-15 20:16:23 +0530

    crate FieldEquipmentDetailsDto

--- src/main/java/lk/ijse/greenshadowbackend/dto/impl/FieldEquipmentDetailsDto.java
+++ src/main/java/lk/ijse/greenshadowbackend/dto/impl/FieldEquipmentDetailsDto.java
@@ -1,4 +1,12 @@
 package lk.ijse.greenshadowbackend.dto.impl;
 
+import lk.ijse.greenshadowbackend.entity.impl.EquipmentEntity;
+import lk.ijse.greenshadowbackend.entity.impl.FieldEntity;
+import lombok.Data;
+
+@Data
 public class FieldEquipmentDetailsDto {
+    private int id;
+    private String fieldId;
+    private String equipmentId;
 }
