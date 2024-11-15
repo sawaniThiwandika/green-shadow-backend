b87e816305f14963c4a87d694d7de6d14d26ea1e
User <thiwandika.whs02@gmail.com>
2024-11-15 21:35:25 +0530

    crate FieldLogDetailsDto

--- src/main/java/lk/ijse/greenshadowbackend/dto/impl/FieldLogDetailsDto.java
+++ src/main/java/lk/ijse/greenshadowbackend/dto/impl/FieldLogDetailsDto.java
@@ -1,5 +1,12 @@
 package lk.ijse.greenshadowbackend.dto.impl;
 
-public class FieldLogDetailsDto {
+import lk.ijse.greenshadowbackend.entity.impl.FieldEntity;
+import lk.ijse.greenshadowbackend.entity.impl.LogEntity;
+import lombok.Data;
 
+@Data
+public class FieldLogDetailsDto {
+    private int id;
+    private String logId;
+    private String fieldId;
 }
