e1d0544824cb802cbc32b1d0e25a28451b64fc07
User <thiwandika.whs02@gmail.com>
2024-11-16 14:09:39 +0530

    create StaffLogDetailsDto

--- src/main/java/lk/ijse/greenshadowbackend/dto/impl/StaffLogDetailsDto.java
+++ src/main/java/lk/ijse/greenshadowbackend/dto/impl/StaffLogDetailsDto.java
@@ -1,5 +1,13 @@
 package lk.ijse.greenshadowbackend.dto.impl;
 
-public class StaffLogDetailsDto {
+import lk.ijse.greenshadowbackend.dto.SuperDto;
+import lk.ijse.greenshadowbackend.entity.impl.LogEntity;
+import lk.ijse.greenshadowbackend.entity.impl.StaffEntity;
+import lombok.Data;
 
+@Data
+public class StaffLogDetailsDto implements SuperDto {
+    private int id;
+    private StaffEntity staffId;
+    private LogEntity logId;
 }
