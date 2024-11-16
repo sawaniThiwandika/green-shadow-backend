6437e1f784811ee0a81346d76ce6b101e6587d58
User <thiwandika.whs02@gmail.com>
2024-11-16 12:39:57 +0530

    crate StaffFieldDetailsDto

--- src/main/java/lk/ijse/greenshadowbackend/dto/impl/StaffFieldDetailsDto.java
+++ src/main/java/lk/ijse/greenshadowbackend/dto/impl/StaffFieldDetailsDto.java
@@ -1,4 +1,13 @@
 package lk.ijse.greenshadowbackend.dto.impl;
 
-public class StaffFieldDetailsDto {
+import lk.ijse.greenshadowbackend.dto.SuperDto;
+import lk.ijse.greenshadowbackend.entity.impl.FieldEntity;
+import lk.ijse.greenshadowbackend.entity.impl.StaffEntity;
+import lombok.Data;
+
+@Data
+public class StaffFieldDetailsDto implements SuperDto {
+    private int id;
+    private String fieldId;
+    private String staffId;
 }
