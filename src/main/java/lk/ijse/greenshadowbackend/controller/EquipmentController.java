55dcc184d615822581bd76d73fce3e8ac6d9f36e
User <thiwandika.whs02@gmail.com>
2024-12-05 18:04:53 +0530

    delete Equipment

--- src/main/java/lk/ijse/greenshadowbackend/controller/EquipmentController.java
+++ src/main/java/lk/ijse/greenshadowbackend/controller/EquipmentController.java
@@ -1,15 +1,19 @@
 package lk.ijse.greenshadowbackend.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import lk.ijse.greenshadowbackend.dto.impl.*;
-import lk.ijse.greenshadowbackend.service.*;
+import lk.ijse.greenshadowbackend.dto.impl.EquipmentDto;
+import lk.ijse.greenshadowbackend.dto.impl.FieldDto;
+import lk.ijse.greenshadowbackend.dto.impl.FieldEquipmentDetailsDto;
+import lk.ijse.greenshadowbackend.dto.impl.StaffEquipmentDetailsDto;
+import lk.ijse.greenshadowbackend.service.EquipmentFieldDetailsService;
+import lk.ijse.greenshadowbackend.service.EquipmentService;
+import lk.ijse.greenshadowbackend.service.EquipmentStaffDetailsService;
 import lk.ijse.greenshadowbackend.util.AppUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -60,13 +64,81 @@ public class EquipmentController {
             equipmentDto.setEquipmentType(type);
             equipmentDto.setStaffEquipmentDetails(staffEquipmentDetailsDtos);
             equipmentDto.setFieldEquipmentDetails(fieldEquipmentDetailsDtos);
-            System.out.println("Equipment dto in  Controller "+ equipmentDto);
+            System.out.println("Equipment dto in  Controller " + equipmentDto);
             equipmentService.saveEquipment(equipmentDto);
             return new ResponseEntity<>(HttpStatus.CREATED);
         } catch (Exception e) {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
     }
+
+    @PutMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<Void> updateEquipment(
+            @RequestPart("equipmentId") String id,
+            @RequestPart("equipmentName") String name,
+            @RequestPart("equipmentType") String type,
+            @RequestPart("equipmentStatus") String status,
+            @RequestPart("equipmentAssignedStaff") String staff,
+            @RequestPart("equipmentAssignedField") String fields) {
+
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            ArrayList<String> fieldList = objectMapper.readValue(
+                    fields,
+                    objectMapper.getTypeFactory().constructCollectionType(ArrayList.class, String.class)
+            );
+            ArrayList<String> staffList = objectMapper.readValue(
+                    staff,
+                    objectMapper.getTypeFactory().constructCollectionType(ArrayList.class, String.class)
+            );
+
+            List<FieldEquipmentDetailsDto> fieldEquipmentDetailsDtos = equipmentFieldDetailsService.getEquipmentFieldDtoList(fieldList,id);
+            List<StaffEquipmentDetailsDto> staffEquipmentDetailsDtos = equipmentStaffDetailsService.getEquipmentStaffDtoList(staffList,id);
+
+
+            EquipmentDto equipmentDto = new EquipmentDto();
+
+            equipmentDto.setEquipmentId(id);
+            equipmentDto.setEquipmentName(name);
+            equipmentDto.setEquipmentStatus(status);
+            equipmentDto.setEquipmentType(type);
+            equipmentDto.setStaffEquipmentDetails(staffEquipmentDetailsDtos);
+            equipmentDto.setFieldEquipmentDetails(fieldEquipmentDetailsDtos);
+            System.out.println("Equipment dto in  Controller " + equipmentDto);
+            equipmentService.updateEquipment(id,equipmentDto);
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<EquipmentDto> getEquipmentList() {
+
+        List<EquipmentDto> equipmentList = equipmentService.getEquipmentList();
+        return equipmentList;
+    }
+
+
+    @DeleteMapping(value = "/{equipmentId}",consumes = MediaType.APPLICATION_JSON_VALUE,produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> deleteEquipment(@PathVariable("equipmentId") String equipmentId){
+
+        if (equipmentId == null || equipmentId.trim().isEmpty()) {
+            return ResponseEntity.badRequest().body("equipmentId is missing");
+        }
+
+        try {
+            System.out.println("Equipmnet to delete:"+equipmentId);
+            equipmentService.deleteEquipment(equipmentId);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+
+    }
+
 
 
 }
--- src/main/java/lk/ijse/greenshadowbackend/service/impl/EquipmentServiceImpl.java
+++ src/main/java/lk/ijse/greenshadowbackend/service/impl/EquipmentServiceImpl.java
@@ -2,16 +2,22 @@ package lk.ijse.greenshadowbackend.service.impl;
 
 import jakarta.transaction.Transactional;
 import lk.ijse.greenshadowbackend.dao.EquipmentDao;
+import lk.ijse.greenshadowbackend.dto.impl.CropDto;
 import lk.ijse.greenshadowbackend.dto.impl.EquipmentDto;
 import lk.ijse.greenshadowbackend.entity.impl.EquipmentEntity;
+import lk.ijse.greenshadowbackend.entity.impl.LogEntity;
 import lk.ijse.greenshadowbackend.service.EquipmentFieldDetailsService;
 import lk.ijse.greenshadowbackend.service.EquipmentService;
 import lk.ijse.greenshadowbackend.service.EquipmentStaffDetailsService;
+import lk.ijse.greenshadowbackend.util.Mapping;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 @Transactional
 @Service
 public class EquipmentServiceImpl implements EquipmentService {
@@ -21,6 +27,8 @@ public class EquipmentServiceImpl implements EquipmentService {
     EquipmentStaffDetailsService equipmentStaffDetailsService;
     @Autowired
     EquipmentFieldDetailsService equipmentFieldDetailsService;
+    @Autowired
+    Mapping mapping;
 
     @Override
     public void saveEquipment(EquipmentDto dto) {
@@ -32,10 +40,10 @@ public class EquipmentServiceImpl implements EquipmentService {
         equipmentEntity.setStaffEquipmentDetails(new ArrayList<>());
         equipmentEntity.setFieldEquipmentDetails(new ArrayList<>());
 
-        // System.out.println("Log Entity  in Log Service "+ logEntity);
+
         System.out.println("Before save " + equipmentEntity.getEquipmentId());
         equipmentDao.save(equipmentEntity);
-        equipmentDao.flush(); // Ensure the entity is saved immediately
+        equipmentDao.flush();
 
         // Save related entities
         boolean saveFieldDetails = equipmentFieldDetailsService.saveEquipmentFieldDetails(equipmentEntity,dto.getFieldEquipmentDetails());
@@ -51,13 +59,35 @@ public class EquipmentServiceImpl implements EquipmentService {
     }
 
     @Override
-    public void updateEquipment(String EquipmentId, EquipmentDto dto) {
+    public void updateEquipment(String equipmentId, EquipmentDto dto) {
+        Optional<EquipmentEntity> byId = equipmentDao.findById(equipmentId);
+        if (byId.isEmpty()) {
+            throw new RuntimeException("Log with ID " + equipmentId + " not found");
+        }
 
+        EquipmentEntity equipmentEntity = byId.get();
+        equipmentEntity.setEquipmentName(dto.getEquipmentName());
+        equipmentEntity.setEquipmentStatus(dto.getEquipmentStatus());
+        equipmentEntity.setEquipmentType(dto.getEquipmentType());
+        equipmentEntity.setEquipmentType(dto.getEquipmentType());
+
+        EquipmentEntity save = equipmentDao.save(equipmentEntity);
+
+        if (save == null){
+            throw new RuntimeException("Log not update!!");
+        }
     }
 
     @Override
     public void deleteEquipment(String id) {
+        System.out.println("EquipmentId "+id);
+        Optional<EquipmentEntity> equipment = equipmentDao.findById(id);
 
+        if (!equipment.isPresent()) {
+            throw new  RuntimeException("Equipment with code " + id + " not found");
+        }
+
+        equipmentDao.deleteById(id);
     }
 
     @Override
@@ -67,7 +97,15 @@ public class EquipmentServiceImpl implements EquipmentService {
 
     @Override
     public List<EquipmentDto> getEquipmentList() {
-        return null;
+
+        List<EquipmentEntity> allEntities = equipmentDao.findAll();
+        if(allEntities.isEmpty()){
+            new RuntimeException("Failed to load");
+        }
+
+        return allEntities.stream()
+                .map(mapping::toEquipmentDto)
+                .collect(Collectors.toList());
     }
 
     @Override
